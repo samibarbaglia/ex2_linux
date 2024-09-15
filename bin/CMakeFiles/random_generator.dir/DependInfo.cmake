b1ed327b9bcfb446e230ca757004f93a1d839121
samibarbaglia <sofia.barbaglia@metropolia.fi>
2024-09-16 02:15:18 +0300

    ex4

---
+++ bin/CMakeFiles/random_generator.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sami/ex2_linux/src/error_check.c" "CMakeFiles/random_generator.dir/src/error_check.c.o" "gcc" "CMakeFiles/random_generator.dir/src/error_check.c.o.d"
+  "/home/sami/ex2_linux/src/generator.c" "CMakeFiles/random_generator.dir/src/generator.c.o" "gcc" "CMakeFiles/random_generator.dir/src/generator.c.o.d"
+  "/home/sami/ex2_linux/src/main.c" "CMakeFiles/random_generator.dir/src/main.c.o" "gcc" "CMakeFiles/random_generator.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/random_generator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/random_generator.dir/src/error_check.c.o"
+  "CMakeFiles/random_generator.dir/src/error_check.c.o.d"
+  "CMakeFiles/random_generator.dir/src/generator.c.o"
+  "CMakeFiles/random_generator.dir/src/generator.c.o.d"
+  "CMakeFiles/random_generator.dir/src/main.c.o"
+  "CMakeFiles/random_generator.dir/src/main.c.o.d"
+  "random_generator"
+  "random_generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/random_generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
